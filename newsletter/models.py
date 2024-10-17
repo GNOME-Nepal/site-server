536869429112f74abf7a1b9a793610e23d833ae9
n1rjal <nirjalpaudel54312@gmail.com>
2024-10-17 18:48:18 +0545

    refactor(newletter): remove subcscription date and single manager

--- newsletter/models.py
+++ newsletter/models.py
@@ -3,12 +3,6 @@ from django.utils import timezone
 import uuid
 
 
-# Custom Manager for active subscribers
-class ActiveSubscriberManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(is_active=True)
-
-
 # Create your models here.
 class NewsletterSubscriber(models.Model):
     created_at = models.DateTimeField(default=timezone.now, editable=False)
@@ -24,16 +18,6 @@ class NewsletterSubscriber(models.Model):
     unsubscribe_token = models.CharField(
         max_length=100, unique=True, default=uuid.uuid4
     )  # Unique unsubscribe token
-
-    subscription_date = models.DateTimeField(
-        default=timezone.now, editable=False
-    )  # Subscription timestamp
-
-    # Use the custom manager
-    objects = models.Manager()  # Default manager
-    active_subscribers = (
-        ActiveSubscriberManager()
-    )  # Manager for querying active subscribers
 
     def __str__(self):
         return self.email
